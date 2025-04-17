5fa982a3f88ba0dbfee34e761221a3cf2a6ee7d0
miguel-s-lopes <miguel.lopes@mywebsite.pt>
2025-04-17 23:41:49 +0100

    Add ngola-way-app/frontend/src/main.jsx 1744929710446

--- ngola-way-app/frontend/src/main.jsx
+++ ngola-way-app/frontend/src/main.jsx
@@ -1,28 +1,19 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom'
 import App from './App'
 import { MapProvider } from './context/MapContext'
 import { AuthProvider } from './context/AuthContext'
 import './index.css'
 
-// Add Google Fonts
-const link = document.createElement('link')
-link.href = 'https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap'
-link.rel = 'stylesheet'
-document.head.appendChild(link)
-
-// Add Font Awesome
-const fontAwesome = document.createElement('link')
-fontAwesome.href = 'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css'
-fontAwesome.rel = 'stylesheet'
-document.head.appendChild(fontAwesome)
-
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <AuthProvider>
-      <MapProvider>
-        <App />
-      </MapProvider>
-    </AuthProvider>
+    <BrowserRouter>
+      <AuthProvider>
+        <MapProvider>
+          <App />
+        </MapProvider>
+      </AuthProvider>
+    </BrowserRouter>
   </React.StrictMode>
 )
